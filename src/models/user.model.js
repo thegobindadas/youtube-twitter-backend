307d24765a7527610c191e67fb9b50bbf400a3c6
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-02-17 00:52:12 +0000

    Add method to check password correctness with bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -57,10 +57,15 @@ userSchema.pre("save", async function(next) {
 
     if(!this.isModified("password")) return next();
 
-    
+
     this.password = await bcrypt.hash(this.password, 10)
     next()
 })
+
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
 
 
 export const User = mongoose.model("User", userSchema)
