8af4b2f8c565bd95bde0dcb57d0020f346a5fb55
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-02-22 01:23:49 +0000

    Implemented function to generate access and refresh tokens for user authentication

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,6 +7,26 @@ import jwt from "jsonwebtoken";
 
 
 
+const generateAccessAndRefreshToken = async (userId) => {
+
+    try {
+        const user = await User.findById(userId)
+    
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+    
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false})
+    
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    }
+}
+
+
+
 const registerUser = asyncHandler(async (req, res) => {
 
     const { fullName, username, email, password } = req.body
