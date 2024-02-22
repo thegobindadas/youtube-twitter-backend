9e28249b8e71be55c1e2ee7ba626a23c5b6a98e7
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-02-22 01:41:38 +0000

    Updated logout functionality to unset refreshToken field in user document

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -165,8 +165,8 @@ const logoutUser = asyncHandler (async (req, res) => {
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {
-                refreshToken: undefined
+            $unset: {
+                refreshToken: 1 // this removes the field from document
             }
         },
         {
@@ -180,7 +180,7 @@ const logoutUser = asyncHandler (async (req, res) => {
         secure: true
     }
 
-    
+
     return res
     .status(200)
     .clearCookie("accessToken", options)
