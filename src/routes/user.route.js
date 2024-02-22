a8210b6e7ee9ac8206a1a2479d9134a7e5e2acca
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-02-22 01:50:29 +0000

    Added POST route for refreshing access token

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,6 +3,7 @@ import {
     registerUser,
     loginUser,
     logoutUser,
+    refreshAccessToken,
     
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -31,7 +32,7 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJwt,  logoutUser)
-
+router.route("/refresh-token").post(refreshAccessToken)
 
 
 
