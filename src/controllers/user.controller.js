b30a2144e30f3236cb5222cba825150953f1e37f
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-02-23 01:20:32 +0000

    Implement endpoint to update user avatar

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -338,6 +338,46 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
 
 
 
+const updateUserAvatar = asyncHandler(async (req, res) => {
+
+    const avatarLocalPath = req.file?.path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file path is missing")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if (!avatar.url) {
+        throw new ApiError(400, "Error while uploading on avatar")
+    }
+
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password")
+
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user,
+            "Avatar image updated successfully"
+        )
+    )
+})
+
+
 
 
 
@@ -351,5 +391,6 @@ export {
     changeCurrentPassword,
     getCurrentUser,
     updateAccountDetails,
+    updateUserAvatar,
     
 }
