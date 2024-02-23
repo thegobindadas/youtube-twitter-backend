d0708b2954b6ad0e34902798ebdd26010fc09354
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-02-23 01:11:13 +0000

    Add endpoint to change current password

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -251,6 +251,42 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
 
 
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+
+    const { oldPassword, newPassword } = req.body;
+
+
+    const user = await User.findById(req.user?._id)
+
+    if (!user) {
+        throw new ApiError(401, "unauthorized user")
+    }
+
+
+    const isPasswordCorrect = user.isPasswordCorrect(oldPassword)
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(401, "invalid old password")
+    }
+
+
+    user.password = newPassword
+    await user.save({ validateBeforeSave: false })
+
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "Password change successfully"
+        )
+    )
+})
+
+
+
 
 
 
@@ -263,5 +299,6 @@ export {
     loginUser,
     logoutUser,
     refreshAccessToken,
-
+    changeCurrentPassword,
+    
 }
