9b8e8d1907d7dfd133809e5442ec39308f111919
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-02-23 01:16:18 +0000

    Add route to retrieve current user information with JWT verification

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,6 +5,7 @@ import {
     logoutUser,
     refreshAccessToken,
     changeCurrentPassword,
+    getCurrentUser,
 
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -35,7 +36,7 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJwt,  logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJwt, changeCurrentPassword)
-
+router.route("/current-user").get(verifyJwt, getCurrentUser)
 
 
 
