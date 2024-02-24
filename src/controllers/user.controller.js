a7f85df095f2824ed4702635ba96c0833b36311c
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-02-24 01:15:05 +0000

    feat: Implement updateUserCoverImage endpoint to handle user cover image updates

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -379,6 +379,46 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
 
 
 
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+
+    const coverImageLocalPath = req.file?.path
+
+    if (!coverImageLocalPath) {
+        throw new ApiError(400, "Cover image file path is missing")
+    }
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!coverImage.url) {
+        throw new ApiError(400, "Error while uploading on Cover image")
+    }
+
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                coverImage: coverImage.url
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password")
+
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user,
+            "cover image updated successfully"
+        )
+    )
+})
+
+
 
 
 
@@ -392,5 +432,6 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
+    updateUserCoverImage,
     
 }
