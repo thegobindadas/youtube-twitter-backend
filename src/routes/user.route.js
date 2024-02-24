b477de935c76beffc330588f8592a0ff4dd0aa26
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-02-24 01:16:54 +0000

    Add PATCH route for updating user cover image

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -8,6 +8,8 @@ import {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
+    updateUserCoverImage,
+
 
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -40,6 +42,15 @@ router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJwt, changeCurrentPassword)
 router.route("/current-user").get(verifyJwt, getCurrentUser)
 router.route("/update-account").patch(verifyJwt, updateAccountDetails)
+
 router.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage)
+
+
+
+
+
+
+
 
 export default router
