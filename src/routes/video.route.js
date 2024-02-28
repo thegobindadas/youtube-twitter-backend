bb04b74a8efdb7ecdffff6a9fa8ef8f68cb91c00
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-02-28 13:17:01 +0000

    Update middleware and function names for consistency

--- src/routes/video.route.js
+++ src/routes/video.route.js
@@ -4,14 +4,14 @@ import {
 
 } from "../controllers/video.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { verifyJWT } from "../middlewares/auth.middleware.js"
+import { verifyJwt } from "../middlewares/auth.middleware.js"
 
 
 
 const router = Router();
 
 
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+router.use(verifyJwt); // Apply verifyJWT middleware to all routes in this file
 
 
 
