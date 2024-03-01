0b2f63f69a573f9f2c2f6a173ba51c15f66c734f
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-03-01 04:16:12 +0000

    Implement endpoint to delete a tweet

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -112,11 +112,50 @@ const updateTweet = asyncHandler(async (req, res) => {
 
 
 
+const deleteTweet = asyncHandler(async (req, res) => {
+
+    const { tweetId } = req.params;
+
+    if (!tweetId) {
+        throw new ApiError(400, "Tweet-id is required")
+    }
+
+
+    const tweet = await Tweet.findById({
+        _id: tweetId
+    });
+
+
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found")
+    }
+
+
+    if (tweet.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "Unauthorized to delete this tweet");
+    }
+
+    
+    await tweet.remove();
+
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            {},
+            "Tweet deleted successfully"
+        )
+    )
+
+})
+
+
+
 
 
 export {
     createTweet,
     getUserTweets,
     updateTweet,
-    
+    deleteTweet
 }
