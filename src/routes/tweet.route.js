f248f3d832bd852116a08dc8e4427e00be711e37
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-03-01 04:15:22 +0000

    Add PATCH route to update tweets by ID

--- src/routes/tweet.route.js
+++ src/routes/tweet.route.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
     createTweet,
     getUserTweets,
+    updateTweet,
     
 } from "../controllers/tweet.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -17,6 +18,10 @@ router.use(verifyJwt);
 router.route("/").post(createTweet);
 
 router.route("/user/:userId").get(getUserTweets);
+
+router
+    .route("/:tweetId")
+    .patch(updateTweet)
 
 
 
