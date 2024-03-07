f95eb492bfef7ec4c03a3aad2f3aceea69230274
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-03-07 07:27:33 +0000

    Added GET route for retrieving liked videos

--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -3,7 +3,7 @@ import {
     toggleVideoLike,
     toggleCommentLike,
     toggleTweetLike,
-
+    getLikedVideos
 } from "../controllers/like.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -18,6 +18,7 @@ router.use(verifyJwt); // Apply verifyJWT middleware to all routes in this file
 router.route("/toggle/v/:videoId").post(toggleVideoLike);
 router.route("/toggle/c/:commentId").post(toggleCommentLike);
 router.route("/toggle/t/:tweetId").post(toggleTweetLike);
+router.route("/videos").get(getLikedVideos);
 
 
 
