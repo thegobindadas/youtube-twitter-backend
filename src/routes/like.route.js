cd202427ff506bae663afc1a183fc91bfa29419b
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-03-07 07:25:36 +0000

    Added POST route for toggling tweet likes

--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
     toggleVideoLike,
     toggleCommentLike,
+    toggleTweetLike,
 
 } from "../controllers/like.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -16,6 +17,7 @@ router.use(verifyJwt); // Apply verifyJWT middleware to all routes in this file
 
 router.route("/toggle/v/:videoId").post(toggleVideoLike);
 router.route("/toggle/c/:commentId").post(toggleCommentLike);
+router.route("/toggle/t/:tweetId").post(toggleTweetLike);
 
 
 
