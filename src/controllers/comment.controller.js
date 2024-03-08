4eb8eefa0a09cb5fba8d37d6f35cc72d32bd8d19
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-03-08 13:57:07 +0000

    feat: Implement endpoint to delete a comment

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -98,11 +98,40 @@ const updateComment = asyncHandler(async (req, res) => {
 
 
 
+const deleteComment = asyncHandler(async (req, res) => {
+    // TODO: delete a comment
+    const { commentId } = req.params;
+
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+        throw new ApiError(404, "Comment not found")
+    }
+
+
+    if (comment.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "Unauthorized to delete this comment")
+    }
+
+    await comment.remove();
+
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            {},
+            "Comment deleted successfully"
+        )
+    )
+})
+
+
+
 
 
 export {
     getVideoComments,
     addComment,
     updateComment,
-    
+    deleteComment
 }
